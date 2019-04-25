18d6dc762568f911d7a7ee2b555604544a75a64a
iM0wgli <49958553+iM0wgli@users.noreply.github.com>
2019-04-25 12:56:09 -0400

    Delete CreateRestorePoint.ps1

--- CreateRestorePoint.ps1
+++
@@ -1,3 +0,0 @@
-### Tested on Windows 10 ###
-Enable-ComputerRestore -Drive "C:\"
-cmd.exe /c "Wmic.exe /Namespace:\\root\default Path SystemRestore Call CreateRestorePoint "BlueBird Restore Point", 100, 12"
